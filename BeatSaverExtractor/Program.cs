3705805314f70fa96f7a4f730bc244d89f34c2a2
Adam ? <adamznow@gmail.com>
2020-12-05 18:07:42 -0800

    Remove unused using

--- BeatSaverExtractor/Program.cs
+++ BeatSaverExtractor/Program.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Threading.Tasks;
 
